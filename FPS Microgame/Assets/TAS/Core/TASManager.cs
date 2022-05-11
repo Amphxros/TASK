8f89b8c6d4b51d4eba504b8c3de8a94b22f18b5a
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-05-11 19:24:22 +0200

    estructura inicial del TASManager

--- FPS Microgame/Assets/TAS/Core/TASManager.cs
+++ FPS Microgame/Assets/TAS/Core/TASManager.cs
@@ -6,11 +6,38 @@ public class TASManager : MonoBehaviour
 {
     [Header("Reading ipnut or replaying input")]
     public GM gameMode_;
+    private static TASManager mInstance_=null;
 
+    public TASManager getInstance(){ return mInstance_;}
+    bool amItheInstance=false;
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        if(mInstance_==null && !amItheInstance){
+            mInstance_=this;
+            amItheInstance=true;
+            DontDestroyOnLoad(this);
+        }
+        else{
+            Destroy(this.gameObject);
+        }
     }
+
+    public void Play(){
+        Debug.Log("playing");
+
+    }
+    public void Pause(){
+        Debug.Log("pause");
+
+    }
+    public void Stop(){
+        Debug.Log("stop");
+
+    }
+    public void Rewind(){
+        Debug.Log("rewind");
+
+    }
+
 }
