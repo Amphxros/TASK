c7bbca0d1e5de2e12c131273172497f315d76e72
Amparo Rubio Bellón <ampxrubio@gmail.com>
2022-05-11 18:28:04 +0200

    TASManagerEditor completo

---
+++ FPS Microgame/Assets/TAS/Core/TASManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TASManager : MonoBehaviour
+{
+    [Header("Reading ipnut or replaying input")]
+    public GM gameMode_;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
